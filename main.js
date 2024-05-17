20417c151c4bf0eedf89f9e6938d3c86a9da38b4
aymenthedeveloper <aymendev.contact@gmail.com>
2024-05-17 09:57:27 +0100

    initial commit, worked on the overall structure and finished working on the nav bar

---
+++ main.js
@@ -0,0 +1,29 @@
+const drobBtn = document.querySelectorAll('.drop-btn');
+const openBtn = document.querySelector('.menu-icon');
+const closeBtn = document.querySelector('.menu-close-icon');
+const topNav = document.querySelector('header nav');
+
+drobBtn.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    btn.classList.toggle('show')
+    drobBtn.forEach((b) => b !== btn && b.classList.remove('show'))
+  })
+})
+
+openBtn.addEventListener('click', () => {
+  topNav.classList.add('show')
+})
+closeBtn.addEventListener('click', () => {
+  topNav.classList.remove('show')
+})
+
+
+window.addEventListener('click', (e) => {
+  if (!e.target.classList.contains('drop-el')){
+    drobBtn.forEach(btn =>btn.classList.remove('show'))
+  }
+  console.log(e.target);
+  if (!e.target.classList.contains('nav-el') && topNav.classList.contains('show')){
+    topNav.classList.remove('show')
+  }
+})
